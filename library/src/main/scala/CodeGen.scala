2023fcf38626bc897d707ce322be2fcf6d90dd72
Eugene Yokota <eed3si9n@gmail.com>
2015-06-04 10:43:05 +0200

    refactoring

--- library/src/main/scala/CodeGen.scala
+++ library/src/main/scala/CodeGen.scala
@@ -18,37 +18,43 @@ $typesCode"""
       val fieldsCode = fields.mkString(",\n  ")
       val fieldNames = td.fields map { field: FieldSchema => field.name }
       val fieldNamesCode = fieldNames.mkString(", ")
-      val fieldNamesEq = fieldNames map { n: String => s"(this.$n == o.$n)" }
-      val fieldNamesEqCode = fieldNamesEq.mkString(" && \n")
-      val fieldNamesHash = fieldNames map { n: String => 
-        s"hash = hash * 31 + this.$n.##"
-      }
-      val fieldNameHashCode = fieldNamesHash.mkString("\n      ") 
       val mainApply =
         s"""def apply($fieldsCode): $name =
     new $name($fieldNamesCode)"""
-      val equals =
-        s"""override def equals(o: Any): Boolean =
-    o match {
-      case x: $name =>
-        $fieldNamesEqCode
-      case _ => false
-    }"""
-      val hashCode =
-        s"""override def hashCode: Int =
-    {
-      var hash = 1
-      $fieldNameHashCode
-      hash
-    }"""
       s"""final class $name($fieldsCode) private {
-  $equals
-  $hashCode
+  ${generateEquals(name, fieldNames)}
+  ${generateHashCode(fieldNames)}
 }
 
 object $name {
   $mainApply
 }"""
+    }
+  
+  def generateEquals(name: String, fieldNames: Vector[String]): String =
+    {
+      val fieldNamesEq = fieldNames map { n: String => s"(this.$n == o.$n)" }
+      val fieldNamesEqCode = fieldNamesEq.mkString(" && \n")
+      s"""override def equals(o: Any): Boolean =
+    o match {
+      case x: $name =>
+        $fieldNamesEqCode
+      case _ => false
+    }"""
+    }
+
+  def generateHashCode(fieldNames: Vector[String]): String =
+    {
+      val fieldNamesHash = fieldNames map { n: String => 
+        s"hash = hash * 31 + this.$n.##"
+      }
+      val fieldNameHashCode = fieldNamesHash.mkString("\n      ") 
+      s"""override def hashCode: Int =
+    {
+      var hash = 1
+      $fieldNameHashCode
+      hash
+    }"""
     }
 
   def generateField(fs: FieldSchema): String =
