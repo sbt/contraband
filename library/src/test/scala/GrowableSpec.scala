0db9bd2e13c6d30dbd87bc04ce2139a783197d1c
Eugene Yokota <eed3si9n@gmail.com>
2015-06-05 11:57:03 +0200

    wip

--- library/src/test/scala/GrowableSpec.scala
+++ library/src/test/scala/GrowableSpec.scala
@@ -12,28 +12,32 @@ class CodeGenSpec extends Specification {
   """
 
   def e1 = {
-    val s = ProtocolSchema.parse(basicSchema)
+    val s = ProtocolSchema.parse(growableSchema)
     val code = CodeGen.generate(s)
     code must_== """package com.example
 
-final class Greeting(message: String) private {
+final class Greeting(message: String,
+  name: String) private {
   override def equals(o: Any): Boolean =
     o match {
       case x: Greeting =>
-        (this.message == o.message)
+        (this.message == o.message) &&
+        (this.name == o.name)
       case _ => false
     }
   override def hashCode: Int =
     {
       var hash = 1
       hash = hash * 31 + this.message.##
+      hash = hash * 31 + this.name.##
       hash
     }
 }
 
 object Greeting {
-  def apply(message: String): Greeting =
-    new Greeting(message)
+  def apply(message: String,
+  name: String): Greeting =
+    new Greeting(message, name)
 }"""
   }
 }
--- library/src/test/scala/SchemaExample.scala
+++ library/src/test/scala/SchemaExample.scala
@@ -11,5 +11,16 @@ object SchemaExample {
       {"name": "message", "type": "string"}]}
   ]
 }"""
-}
 
+  val growableSchema = """{
+  "namespace": "com.example",
+  "protocol": "HelloWorld",
+  "doc": "Protocol Greetings",
+
+  "types": [
+    {"name": "Greeting", "type": "record", "fields": [
+      {"name": "message", "type": "string", "since": "0.1.0"},
+      {"name": "name", "type": "string", "since": "0.2.0"}]}
+  ]
+}"""
+}
