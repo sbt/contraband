3414ef3b2f83834db5625eb2450d7bb5df08b2f3
Antonio Cunei <antonio.cunei@typesafe.com>
2015-06-05 17:50:22 +0200

    Updates to scripted testing

--- library/src/sbt-test/sbt-datatype/simple/project/Build.scala
+++ library/src/sbt-test/sbt-datatype/simple/project/Build.scala
@@ -7,6 +7,11 @@ trait BcResult
 case class RuntimeFailure(proj: String) extends BcResult
 case class Success(proj: String) extends BcResult
 
+object BcBuild extends Build {
+//  def defaultSettings: Seq[Setting[_]] = BcTests.settings
+  val root = Project("root", file(".")).configs(BcTests.SecondCompile).settings(BcTests.settings:_*)
+}
+
 object BcTests {
   val SecondCompile = Configuration("second-compile", "The second compilation", true, List(Compile), true)
   val runBc = TaskKey[BcResult]("bc-run")
@@ -23,7 +28,10 @@ object BcTests {
       sourceGenerators in SecondCompile += (genSource in SecondCompile).taskValue,
       unmanagedClasspath in SecondCompile <<= fullClasspath in Compile,
       mainClass := Some("Main"),
-      runBc <<= (name, fullClasspath in SecondCompile, mainClass, runner in run, streams) map runTask
+      runBc <<= (name, fullClasspath in SecondCompile, mainClass, runner in run, streams) map runTask,
+      version := "0.1",
+      scalaVersion := "2.11.6",
+      name := "simpletest"
     )
   def runTask(name: String, classpath: Classpath, mainClass: Option[String], runner: ScalaRun, s: TaskStreams): BcResult =
     runner.run(mainClass getOrElse "Main", classpath map (_.data), Seq.empty, s.log) match {
