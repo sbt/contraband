3da574e080ca2820380f562b98bf12aeb0642708
Antonio Cunei <antonio.cunei@typesafe.com>
2015-06-05 16:35:59 +0200

    Adding scripted binary compatibility test, commit 1/2

---
+++ library/src/sbt-test/sbt-datatype/simple/project/Build.scala
@@ -0,0 +1,33 @@
+package sbt.datatype
+import SchemaExample._
+import sbt._
+import Keys._
+
+trait BcResult
+case class RuntimeFailure(proj: String) extends BcResult
+case class Success(proj: String) extends BcResult
+
+object BcTests {
+  val SecondCompile = Configuration("second-compile", "The second compilation", true, List(Compile), true)
+  val runBc = TaskKey[BcResult]("bc-run")
+  val genSource = TaskKey[Seq[File]]("genSource")
+  def settings: Seq[Setting[_]] =
+    inConfig(SecondCompile)(Defaults.compileSettings) ++
+    Seq(
+//      scalaSource in SecondCompile <<= sourceDirectory in Compile apply (_ / "scala1"),
+      genSource in SecondCompile := {
+        val file = sourceManaged.value / "Basic.scala"
+        IO.write(file, CodeGen.generate(ProtocolSchema.parse(basicSchema)))
+        Seq(file)
+      },
+      sourceGenerators in SecondCompile += (genSource in SecondCompile).taskValue,
+      unmanagedClasspath in SecondCompile <<= fullClasspath in Compile,
+      mainClass := Some("Main"),
+      runBc <<= (name, fullClasspath in SecondCompile, mainClass, runner in run, streams) map runTask
+    )
+  def runTask(name: String, classpath: Classpath, mainClass: Option[String], runner: ScalaRun, s: TaskStreams): BcResult =
+    runner.run(mainClass getOrElse "Main", classpath map (_.data), Seq.empty, s.log) match {
+       case Some(msg) => Success(name)
+       case None      => RuntimeFailure(name)
+    }
+}
---
+++ library/src/sbt-test/sbt-datatype/simple/project/plugins.sbt
@@ -0,0 +1,4 @@
+libraryDependencies ++= Seq(
+  "org.scala-sbt" %% "datatype" % "0.1.0-SNAPSHOT",
+  "org.scala-sbt" %% "datatype" % "0.1.0-SNAPSHOT" classifier "tests"
+)
