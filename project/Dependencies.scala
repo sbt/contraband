6814c0f2cfc196c52b6d3e05b89649021f01e3be
Eugene Yokota <eed3si9n@gmail.com>
2017-07-01 04:54:05 -0400

    switch json4s

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,18 +1,25 @@
 import sbt._
+import Keys._
 
 object Dependencies {
   val jawnVersion = "0.10.4"
 
-  private val jsonTuples = Seq(
-    // Keep json4s with this version.
-    ("org.json4s", "json4s-core", "3.2.10"),
-    ("org.spire-math", "jawn-parser", jawnVersion),
-    ("org.spire-math", "jawn-json4s", jawnVersion)
-  )
-
-  val jsonDependencies = jsonTuples map {
-    case (group, mod, version) => (group %% mod % version).exclude("org.scala-lang", "scalap")
-  }
+  val jsonDependencies =
+    Def.setting {
+      val jsonTuples = Seq(
+        // we are going to use older json4s intentionally to match bintry use etc.
+        ("org.json4s", "json4s-core", 
+        scalaBinaryVersion.value match {
+          case "2.10" | "2.11" => "3.2.10"
+          case _               => "3.4.2"
+        }),
+        ("org.spire-math", "jawn-parser", jawnVersion),
+        ("org.spire-math", "jawn-json4s", jawnVersion)
+      )
+      jsonTuples map {
+        case (group, mod, version) => (group %% mod % version).exclude("org.scala-lang", "scalap")
+      }
+    }
 
   val scalaTest         = "org.scalatest" %% "scalatest" % "3.0.0"
   val parboiled         = "org.parboiled" %% "parboiled" % "2.1.3"
