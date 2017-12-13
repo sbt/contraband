2ad038b1cd2ae6e8d1541aac641cf0ce3aab689f
Eugene Yokota <eed3si9n@gmail.com>
2017-12-12 22:12:41 -0500

    update to sjson-new 0.8.2

--- plugin/src/main/scala/ContrabandPlugin.scala
+++ plugin/src/main/scala/ContrabandPlugin.scala
@@ -103,7 +103,7 @@ object ContrabandPlugin extends AutoPlugin {
     )
 
   override def globalSettings = Seq(
-    contrabandSjsonNewVersion := "0.8.1"
+    contrabandSjsonNewVersion := "0.8.2"
   )
 }
 
