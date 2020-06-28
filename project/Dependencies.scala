365aa0fcf4cc7649e6d97372beb0669479822b9b
kenji yoshida <6b656e6a69@gmail.com>
2020-06-28 15:03:26 +0900

    parboiled 2.1.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -36,7 +36,7 @@ object Dependencies {
     }
 
   val scalaTest         = "org.scalatest" %% "scalatest" % "3.2.0"
-  val parboiled         = "org.parboiled" %% "parboiled" % "2.1.7"
+  val parboiled         = "org.parboiled" %% "parboiled" % "2.1.8"
   val diffutils         = "com.googlecode.java-diff-utils" % "diffutils" % "1.3.0"
   val verify            = "com.eed3si9n.verify" %% "verify" % "0.2.0"
 }
