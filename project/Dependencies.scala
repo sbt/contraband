24e439be0d024b8dff723b715e7b9e2392aeec59
xuwei-k <6b656e6a69@gmail.com>
2022-09-25 11:08:29 +0900

    Scala 3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,10 +4,11 @@ import Keys._
 object Dependencies {
   val scala212 = "2.12.15"
   val scala213 = "2.13.8"
+  val scala3 = "3.3.0"
 
-  val sjsonNewScalaJson = "com.eed3si9n" %% "sjson-new-scalajson" % "0.9.0"
-  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.2"
-  val parboiled = "org.parboiled" %% "parboiled" % "2.1.8"
+  val sjsonNewScalaJson = "com.eed3si9n" %% "sjson-new-scalajson" % "0.10.0"
+  val scalaTest = "org.scalatest" %% "scalatest" % "3.2.16"
+  val parboiled = "org.parboiled" %% "parboiled" % "2.5.0"
   val diffutils = "com.googlecode.java-diff-utils" % "diffutils" % "1.3.0"
   val verify = "com.eed3si9n.verify" %% "verify" % "1.0.0"
 }
