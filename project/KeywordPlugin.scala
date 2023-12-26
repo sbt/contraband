1cead74bd539550417a4992780eee8803af270a4
xuwei-k <6b656e6a69@gmail.com>
2023-12-26 12:54:21 +0900

    Update scalafmt

--- project/KeywordPlugin.scala
+++ project/KeywordPlugin.scala
@@ -7,28 +7,28 @@ object KeywordPlugin extends AutoPlugin {
   lazy val scalaKeywords = TaskKey[Set[String]]("scala-keywords")
   lazy val generateKeywords = TaskKey[File]("generateKeywords")
 
-  def getScalaKeywords: Set[String] =
-    {
-      val g = new scala.tools.nsc.Global(new scala.tools.nsc.Settings)
-      g.nme.keywords.map(_.toString)
-    }
-  def writeScalaKeywords(base: File, keywords: Set[String]): File =
-    {
-      val init = keywords.toList.sortBy(identity).map(tn => '"' + tn + '"').mkString("Set(", ", ", ")")
-      val objectName = "ScalaKeywords"
-      val packageName = "sbt.contraband"
-      val keywordsSrc =
-        s"""package $packageName
+  def getScalaKeywords: Set[String] = {
+    val g = new scala.tools.nsc.Global(new scala.tools.nsc.Settings)
+    g.nme.keywords.map(_.toString)
+  }
+  def writeScalaKeywords(base: File, keywords: Set[String]): File = {
+    val init = keywords.toList.sortBy(identity).map(tn => '"' + tn + '"').mkString("Set(", ", ", ")")
+    val objectName = "ScalaKeywords"
+    val packageName = "sbt.contraband"
+    val keywordsSrc =
+      s"""package $packageName
            |object $objectName {
            |  val values = $init
            |}""".stripMargin
-      val out = base / packageName.replace('.', '/') / (objectName + ".scala")
-      IO.write(out, keywordsSrc)
-      out
-    }
-  override def projectSettings: Seq[Setting[_]] = inConfig(Compile)(Seq(
-    scalaKeywords := getScalaKeywords,
-    generateKeywords := writeScalaKeywords(sourceManaged.value, scalaKeywords.value),
-    sourceGenerators += Def.task(Seq(generateKeywords.value)).taskValue
-  ))
+    val out = base / packageName.replace('.', '/') / (objectName + ".scala")
+    IO.write(out, keywordsSrc)
+    out
+  }
+  override def projectSettings: Seq[Setting[_]] = inConfig(Compile)(
+    Seq(
+      scalaKeywords := getScalaKeywords,
+      generateKeywords := writeScalaKeywords(sourceManaged.value, scalaKeywords.value),
+      sourceGenerators += Def.task(Seq(generateKeywords.value)).taskValue
+    )
+  )
 }
