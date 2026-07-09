82e54b7b643296d409747be5aa576cad268c8873
xuwei-k <6b656e6a69@gmail.com>
2026-07-09 20:52:25 +0900

    Avoid deprecated scala.App
    https://github.com/scala/scala3/blob/3.8.4/library/src/scala/App.scala#L65

--- plugin/src/sbt-test/sbt-contraband/roundtrip-test-java/src/main/scala/com/foo/Example.scala
+++ plugin/src/sbt-test/sbt-contraband/roundtrip-test-java/src/main/scala/com/foo/Example.scala
@@ -5,22 +5,25 @@ import sjsonnew.JsonFormat
 import sjsonnew.support.scalajson.unsafe.{ Converter, CompactPrinter }
 import com.example._
 
-object Example extends App {
+object Example {
   import generated.CustomProtocol._
-  val g0: Greeting = SimpleGreeting.of("Hello")
-  val g1: Greeting = SimpleGreeting.of("Hello", Optional.empty[Integer]())
-  val g21: Greeting = GreetingWithAttachments.of("Hello", Array.empty)
-  val g3: Greeting = GreetingWithOption.of("Hello", Optional.ofNullable("foo"))
-  val gl1: GreetingList = GreetingList.of(Array(g0))
-  val gl2: GreetingList = GreetingList.of(Array(g0))
 
-  println(CompactPrinter(Converter.toJson(g0).get))
-  println(Converter.fromJson[Greeting](Converter.toJson(g0).get).get)
+  def main(args: Array[String]): Unit = {
+    val g0: Greeting = SimpleGreeting.of("Hello")
+    val g1: Greeting = SimpleGreeting.of("Hello", Optional.empty[Integer]())
+    val g21: Greeting = GreetingWithAttachments.of("Hello", Array.empty)
+    val g3: Greeting = GreetingWithOption.of("Hello", Optional.ofNullable("foo"))
+    val gl1: GreetingList = GreetingList.of(Array(g0))
+    val gl2: GreetingList = GreetingList.of(Array(g0))
 
-  assert(Converter.fromJson[Greeting](Converter.toJson(g0).get).get == g0)
-  assert(Converter.fromJson[Greeting](Converter.toJson(g1).get).get == g1)
-  assert(Converter.fromJson[Greeting](Converter.toJson(g21).get).get == g21)
-  assert(Converter.fromJson[Greeting](Converter.toJson(g3).get).get == g3)
-  assert(gl1 == gl2)
-  assert(gl1.## == gl2.##)
+    println(CompactPrinter(Converter.toJson(g0).get))
+    println(Converter.fromJson[Greeting](Converter.toJson(g0).get).get)
+
+    assert(Converter.fromJson[Greeting](Converter.toJson(g0).get).get == g0)
+    assert(Converter.fromJson[Greeting](Converter.toJson(g1).get).get == g1)
+    assert(Converter.fromJson[Greeting](Converter.toJson(g21).get).get == g21)
+    assert(Converter.fromJson[Greeting](Converter.toJson(g3).get).get == g3)
+    assert(gl1 == gl2)
+    assert(gl1.## == gl2.##)
+  }
 }
